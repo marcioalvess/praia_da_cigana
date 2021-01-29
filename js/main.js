59c80aafddef1933db88dbad77230564884c717c
Márcio Alves <74689935+marcioalvess@users.noreply.github.com>
2021-01-29 00:45:02 -0300

    alterando menu

--- js/main.js
+++ js/main.js
@@ -3,49 +3,55 @@ function load(){
     alert("Pagina carregada!")
 }
 
-function redir_acesso(elemento){
-    window.open("/form.html")
-}
+$(document).ready(function() {
+    var botao = $('.bt1');
+    var dropDown = $('.ul-entret');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+    });
+});
 
-function abrir(elemento){
-    document.getElementById("historico").innerHTML= "Histórico";
-    document.getElementById("fotos").innerHTML= "Fotos";
-    document.getElementById("cardapio").innerHTML= "Cardápio";
-}
+$(document).ready(function() {
+    var botao = $('.bt2');
+    var dropDown = $('.ul-cardapio');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+    });
+});
 
-
-function abrir2(elemento){
-    document.getElementById("feijoada").innerHTML= "Feijoada";
-    document.getElementById("festival-sorvete").innerHTML= "Festiva de Sorvete";
-    document.getElementById("corrida-canoa").innerHTML= "Corrida de Canoa";
-}
-
-function abrir3(elemento){
-    document.getElementById("peixe").innerHTML= "Peixe Frito";
-    document.getElementById("suco").innerHTML= "";
-    document.getElementById("carne").innerHTML= "Corrida de Canoa";
-    
-    function menu_suco(elemento){
-    console.log(elemento.value)//value puxa um valor
-    }
-}
-
-function abrir4(elemento){
-    window.open("https://www.google.com.br/maps/place/Praia+da+Cigana/@-6.6051451,-47.4566174,17z/data=!3m1!4b1!4m5!3m4!1s0x92da09fb850e84d3:0x25e7f0528bd0d746!8m2!3d-6.6051504!4d-47.4544287");
-}
-
+$(document).ready(function() {
+    var botao = $('.bt3');
+    var dropDown = $('.ul-sobre');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+    });
+});
 
 
 function redir_historico(elemento){
     window.open("/historico.html");//pra abrir a pagina interna basta colocar a barra e o nome da pasta
 }
-function redir_fotos(elemento){
+
+function redir_acesso(elemento){
+    window.open("/form.html");
+}
+
+function redir_localiza(elemento){
+    window.open ("/https://www.google.com/maps/place/Praia+da+Cigana/@-6.6051504,-47.4566174,17z/data=!3m1!4b1!4m5!3m4!1s0x92da09fb850e84d3:0x25e7f0528bd0d746!8m2!3d-6.6051504!4d-47.4544287");
+}   
+/* function redir_fotos(elemento){
     window.open("/album.html");
 }
 function redir_cardapio(elemento){
     window.open("https://digitalinnovation.one/");
 }
-
+ */
 
 /* function trocar(){
     //document.getElementById("mousemove").innerHTML = "Obrigado por pasar o mouse";
